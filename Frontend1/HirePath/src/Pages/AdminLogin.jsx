7bb6e73510c37058de021aaab89d838593d68edd
SHIVAM KUMAR RAJ <167607479+Shivamraj-07@users.noreply.github.com>
2025-01-11 20:18:00 +0530

    Update AdminLogin.jsx

--- Frontend1/HirePath/src/Pages/AdminLogin.jsx
+++ Frontend1/HirePath/src/Pages/AdminLogin.jsx
@@ -9,7 +9,7 @@ const AdminLogin = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://localhost:5000/auth/login', {
+      const response = await fetch('https://update-productio-project-backend.onrender.com/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
